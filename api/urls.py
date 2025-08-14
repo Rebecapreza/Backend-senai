75f79389500889bef25a6841550e4efa9e819be7
unknown <ds@ESN501D1278484.educ501.local>
2025-08-14 16:55:56 -0300

    14-08

--- api/urls.py
+++ api/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .views import * 
 urlpatterns = [
     path ('Autores', AutoresView.as_view()),
+    path ('autoeres', visualizacao_autor)
 ]
--- api/views.py
+++ api/views.py
@@ -2,8 +2,27 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView
 from .models import Autores
 from .serializers import AutorSerializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
 
 class AutoresView(ListCreateAPIView):
     queryset = Autores.objects.all()
     serializer_class = AutorSerializers
+
+@api_view(['GET', "POST"])
+def visualizacao_autor(request):
+    if request.method == 'GET':
+        queryset = Autores.objects.all()
+        serializer = AutorSerializers(queryset, many = True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = AutorSerializers (data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+        
 
