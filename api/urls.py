e221f03a8258b842fd618cf85084e0ddcf9f0309
unknown <ds@ESN501D1278484.educ501.local>
2025-08-28 16:50:47 -0300

    não sei mais

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,24 @@
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
+    #get/post
     path ('Autores', AutoresView.as_view()),
     path ('autoeres', visualizacao_autor),
     path ('Editora', EditoraView.as_view()),
     path ('Livro', LivroView.as_view()),
+    path ('Buscar', AutoresView.as_view()),
 
+    #update/ delete
     path ('autores/<int:pk>', AutoresDetailView.as_view()),
     path ('Editora/<int:pk>', EditoraDetailView.as_view()),
-    path ('Livro/<int:pk>', LivroDetailView.as_view())
+    path ('Livro/<int:pk>', LivroDetailView.as_view()),
+
+    #token
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- api/views.py
+++ api/views.py
@@ -2,37 +2,50 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Autores, Editora, Livro
 from .serializers import AutorSerializers, EditoraSerializers, LivroSerializers
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 
 class AutoresView(ListCreateAPIView):
     queryset = Autores.objects.all()
     serializer_class = AutorSerializers
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['id']
+    search_fields = ['nome']
 
 class AutoresDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Autores.objects.all()
     serializer_class = AutorSerializers
+    permission_classes = [IsAuthenticated]
 
 class EditoraView(ListCreateAPIView):
     queryset = Editora.objects.all()
     serializer_class = EditoraSerializers
+    permission_classes = [IsAuthenticated]
 
 class EditoraDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Editora.objects.all()
     serializer_class = AutorSerializers
+    permission_classes = [IsAuthenticated]
 
 class LivroView(ListCreateAPIView):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializers
+    permission_classes = [IsAuthenticated]
 
 class LivroDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializers
+    permission_classes = [IsAuthenticated]
 
 
 @api_view(['GET', "POST"])
+@permission_classes([IsAuthenticated])
 def visualizacao_autor(request):
     if request.method == 'GET':
         queryset = Autores.objects.all()
